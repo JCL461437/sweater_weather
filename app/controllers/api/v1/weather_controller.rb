a6a82f3958585b5ce25ad3b1e9f6b01ca8eb5506
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-22 16:36:05 -0600

    Add updated weather_controller:

--- app/controllers/api/v1/weather_controller.rb
+++ app/controllers/api/v1/weather_controller.rb
@@ -2,10 +2,15 @@ class Api:V1:WeatherController < ApplicationController
   def forcast  
     location = params[:location] # pulled from query params for `/api/v1/forecast?location=Salt Lake City, UT`
     # {"lat":40.76031,"lng":-111.88822}
-    @current_weather = WeatherApiService.current_weather(location)
-    @five_days_forcast = WeatherApiService.get_five_days_forcast(location)
+
+    # turns location from query params into lat and long using Map Quest API
+    lat_long =  MapQuestApiService.get_lat_long(location)
+
+    # Retrieves current weather and forcast for hourly and five days using Weather API
+    @current_weather = WeatherApiService.current_weather(lat_long)
+    @five_days_forcast = WeatherApiService.get_five_days_forcast(lat_long)
     # @hourly_weather = WeatherApiService.get_hourly_forcast
 
-    render json: CurrentWeatherSerializer.all_weather(@current_weather, @five_days_forcast)
+    render json: CurrentWeatherSerializer.all_weather(@current_weather)
   end
 end

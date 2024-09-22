c8264ac2b3e90941b475b147e251ebe903aca997
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-22 13:22:30 -0600

    Add instance variables for the @current_weather and @five_days_forcast in the Weather Controllers forcast action

--- app/controllers/api/v1/weather_controller.rb
+++ app/controllers/api/v1/weather_controller.rb
@@ -1,6 +1,8 @@
 class Api:V1:WeatherController < ApplicationController
   def forcast  
     location = params[:location] # pulled from query params for `/api/v1/forecast?location=Salt Lake City, UT`
-    
+
+    @current_weather = WeatherApiService.current_weather(location)
+    @five_days_forcast = WeatherApiService.five_days_forcast(location)
   end
 end

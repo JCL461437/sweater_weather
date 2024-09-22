0a64906928c3c5b04e9e807cd249d5e83b64cca0
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-22 16:28:53 -0600

    Test for current weather poros

--- spec/poros/weather_spec.rb
+++ spec/poros/weather_spec.rb
@@ -1,21 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe Weather do
+RSpec.describe CurrentWeather do
   describe '#initialize' do
     it 'instantiates a CurrentWeather object with the correct attributes from the JSON response' do
-      json = File.read('spec/fixtures/current_slc_weather.json')
+      json_data = File.read('spec/fixtures/current_slc_weather.json')
+      parsed_data = JSON.parse(json_data, symbolize_names: true)
+      
+      current_weather = CurrentWeather.new(parsed_data)
 
-      current_weather = CurrentWeather.new(json)
+      expect(current_weather).to be_a(CurrentWeather)
+      expect(current_weather.location).to eq(parsed_data[:location])
+      expect(current_weather.name).to eq(parsed_data[:location][:name])
+      expect(current_weather.region).to eq(parsed_data[:location][:region])
+      expect(current_weather.lat).to eq(parsed_data[:location][:lat])
+      expect(current_weather.lon).to eq(parsed_data[:location][:lon])
+      expect(current_weather.timezone).to eq(parsed_data[:location][:tz_id])
+      expect(current_weather.localtime).to eq(parsed_data[:location][:localtime])
 
-      expect(current_weather).to be_a CurrentWeather
-      expect(current_weather.temperature).to eq(72.5)
-      expect(current_weather.last_updated).to eq("2023-08-03 15:30")
-      expect(current_weather.condition_text).to eq("Partly cloudy")
-      expect(current_weather.condition_icon).to eq("//cdn.weatherapi.com/weather/64x64/day/116.png")
-      expect(current_weather.feels_like).to eq(71.2)
-      expect(current_weather.uvi).to eq(5)
-      expect(current_weather.visibility).to eq(6)
-      expect(current_weather.humidity).to eq(65)
+      expect(current_weather.current).to eq(parsed_data[:current])
+      expect(current_weather.last_updated).to eq(parsed_data[:current][:last_updated])
+      expect(current_weather.temperature).to eq(parsed_data[:current][:temp_f])
+      expect(current_weather.condition).to eq(parsed_data[:current][:condition])
+      expect(current_weather.text).to eq(parsed_data[:current][:condition][:text])
+      expect(current_weather.icon).to eq(parsed_data[:current][:condition][:icon])
+      expect(current_weather.humidity).to eq(parsed_data[:current][:humidity])
+      expect(current_weather.feels_like).to eq(parsed_data[:current][:feelslike_f])
+      expect(current_weather.visibility).to eq(parsed_data[:current][:vis_miles])
+      expect(current_weather.uv).to eq(parsed_data[:current][:uv])
     end
   end
 end

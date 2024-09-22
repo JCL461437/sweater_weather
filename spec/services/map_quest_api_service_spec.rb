b02acafd163f481a159332b832669d6dd19bcd9f
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-22 10:48:45 -0600

    Add a map_quest_api_service_spec with a test for the #get_lat_long method

---
+++ spec/services/map_quest_api_service_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe MapQuestApiService do
+  it 'can #get_lat_long' do
+
+    query = MapQuestApiService.get_lat_long("Salt Lake City, UT")
+    map_quest = query.first
+
+    expect(query).to be_a Array
+    expect(map_quest).to be_a Hash
+
+    expect(map_quest).to have_key("info")
+    expect(map_quest["info"]).to be_a(String)
+
+    expect(map_quest).to have_key("results")
+    expect(map_quest["results"]).to be_a(String)
+
+    expect(map_quest).to have_key("locations")
+    expect(map_quest["results"][1]["locations"]).to be_a(String)
+
+    expect(map_quest).to have_key("displayLatLng")
+    expect(map_quest["results"][1]["locations"][1]["latLng"]).to be_a(Hash)
+  end
+end

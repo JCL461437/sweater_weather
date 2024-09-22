ce105e72c3dfb6a96cda9979a1e159978c1bb5d2
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-22 10:16:00 -0600

    Fix the name of the map_quest_api_service.rb class to be MapQuest and will add the class method for get_lat_long from the map quest api

--- app/services/map_quest_api_service.rb
+++ app/services/map_quest_api_service.rb
@@ -1,9 +1,18 @@
-class WeatherApiService
+class MapQuestApiService
+
+  BASE_URL = "https://www.mapquestapi.com"
 
   def self.conn 
-    Faraday.new(url: "https://www.mapquestapi.com/geocoding/v1/address") do |faraday|
+    Faraday.new(url: BASE_URL) do |faraday|
       faraday.params['key'] = Rails.application.credentials.map_quest[:key]
       faraday.adapter Faraday.default_adapter
     end
+  end
+
+  def self.get_lat_long(location)
+    connection = Faraday.new(url: BASE_URL)
+
+    response = connection.get("geocoding/v1/address?key=#{Rails.application.credentials.map_quest[:key]}&location=#{location}")
+    JSON.parse(response.body, symbolize_names: true)
   end
 end

15a1ca5230a3b3db2c7d4e1026294c972cdca42e
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-22 10:26:00 -0600

    Refactor the services/weather_api_service.rb to use BASE_URL

--- app/services/weather_api_service.rb
+++ app/services/weather_api_service.rb
@@ -1,7 +1,9 @@
 class WeatherApiService
 
+  BASE_URL = "http://api.weatherapi.com"
+
   def self.conn 
-    Faraday.new(url: "http://api.weatherapi.com") do |faraday|
+    Faraday.new(url: BASE_URL) do |faraday|
       # faraday.headers['X-Api-Key'] = Rails.application.credentials.weather[:key]
       faraday.params['key'] = Rails.application.credentials.weather[:key]
       faraday.adapter Faraday.default_adapter

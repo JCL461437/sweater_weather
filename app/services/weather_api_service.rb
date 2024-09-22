6c54719ba63a76d0c65d15116b484c1c4f45c016
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-22 13:22:58 -0600

    Add weather_api_service.rb class method for five_days_forcast and current_weather

--- app/services/weather_api_service.rb
+++ app/services/weather_api_service.rb
@@ -9,4 +9,16 @@ class WeatherApiService
       faraday.adapter Faraday.default_adapter
     end
   end
+
+  def self.current_weather(location)
+    response = connection.get("/v1/current.json?key=#{Rails.application.credentials.weather[:key]}&q=#{location}") # do I need to send it in query params because of line 6-8?
+    
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.five_days_forcast(location)
+    response = connection.get("/v1/forecast.json?key=#{Rails.application.credentials.weather[:key]}&q=#{location}&days=5") # do I need to send it in query params because of line 6-8?
+    
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end

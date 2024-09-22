d69514e0ebc350bdb6fa184caf154f47021d6d27
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-22 12:44:41 -0600

    Add loaction variable pulling value from the query params for endpoint

--- app/controllers/api/v1/weather_controller.rb
+++ app/controllers/api/v1/weather_controller.rb
@@ -1,5 +1,6 @@
 class Api:V1:WeatherController < ApplicationController
   def forcast  
+    location = params[:location] # pulled from query params for `/api/v1/forecast?location=Salt Lake City, UT`
     
   end
 end

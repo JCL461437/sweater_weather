8fe7836656d50443188c0d3c41b0d2adb903eac3
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-22 11:19:59 -0600

    Refactor the map_quest_api_service spec to expect that key value pairs are symbolized, not strings, and removed faulty expectations

--- spec/services/map_quest_api_service_spec.rb
+++ spec/services/map_quest_api_service_spec.rb
@@ -4,22 +4,20 @@ RSpec.describe MapQuestApiService do
   it 'can #get_lat_long' do
 
     query = MapQuestApiService.get_lat_long("Salt Lake City, UT")
-    map_quest = query.first
-
-    expect(query).to be_a Array
-    expect(map_quest).to be_a Hash
+  
+    expect(query).to be_a Hash
     binding.pry
-    expect(map_quest).to have_key("info")
-    expect(map_quest["info"]).to be_a(String)
+    expect(query).to have_key(:info)
+    expect(query[:info]).to be_a(String)
     binding.pry
-    expect(map_quest).to have_key("results")
-    expect(map_quest["results"]).to be_a(String)
+    expect(query).to have_key(:results)
+    expect(query[:results]).to be_a(String)
     binding.pry
-    expect(map_quest).to have_key("locations")
-    expect(map_quest["results"][1]["locations"]).to be_a(String)
+    expect(query).to have_key(:locations)
+    expect(query[:results][1][:locations]).to be_a(String)
     binding.pry
-    expect(map_quest).to have_key("displayLatLng")
-    expect(map_quest["results"][1]["locations"][1]["latLng"]).to be_a(Hash)
+    expect(query).to have_key(:displayLatLng)
+    expect(query[:results][1][:locations][1][:latLng]).to be_a(Hash)
     binding.pry
   end
 end

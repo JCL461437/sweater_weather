0867a10e0871a6e34c2bff2070e7f05ff2b0845a
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-22 17:38:46 -0600

    Add spec for weather facade

---
+++ spec/facades/weather_facade_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+RSpec.describe WeatherFacade do
+  it "will make a poros from the json using the current_weather method" do
+    lat_long = "40.76031,-111.88822" #slc lat long values 
+
+    response = WeatherFacade.new.current_weather(lat_long)
+
+    expect(response).to be_a CurrentWeather
+  end
+  
+  it "will make a FiveDaysForecastWeather from the json using the five_days_forecast method" do
+    lat_long = "40.76031,-111.88822"
+
+    response = WeatherFacade.new.five_days_forecast(lat_long)
+
+    expect(response).to be_a Array
+
+    expect(response[0]).to be_a FiveDaysForecastWeather
+
+    expect(response.count).to eq 5
+  end
+
+  it "will make a HourForecastWeather poros from the json using the get_hourly_forecast method" do
+    lat_long = "40.76031,-111.88822"
+
+    response = WeatherFacade.new.five_days_forecast(lat_long)
+
+    expect(response).to be_a Array
+
+    expect(response[0]).to be_a HourForecastedWeather
+
+    expect(response.count).to eq 24
+  end
+end

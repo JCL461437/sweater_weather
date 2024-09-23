d52ab9ce3df86232902fb366ba85c73964676868
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-23 09:47:05 -0600

    Add changes to call BookWeatherSerializer in the book-search action in the Book Controller. Also added use of weather and mapquest facade so that a forcast fo the days weather could be retrieved

--- app/controllers/api/v1/book_controller.rb
+++ app/controllers/api/v1/book_controller.rb
@@ -4,7 +4,10 @@ class Api::V1::BookController < ApplicationController
     quantity = params[:quantity]
 
     books = BookFacade.new.get_books(location, quantity)
+
+    lat_long = MapQuestFacade.new.lat_long(location)
+    weather_forecast = WeatherFacade.new.five_days_forecast(lat_long)
     
-    render json: BookSerializer.the_books(books)
+    render json: BookWeatherSerializer.books_and_weather(books, weather_forecast)
   end
 end

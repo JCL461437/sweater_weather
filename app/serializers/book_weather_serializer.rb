8c3280c2c9fb274d4c2df278fa380669dcf04a05
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-23 11:07:38 -0600

    Add book_weather_serializer with formatted json response

--- app/serializers/book_weather_serializer.rb
+++ app/serializers/book_weather_serializer.rb
@@ -1,7 +1,31 @@
 class BookWeatherSerializer
   include JSONAPI::Serializer
-
+  
   def self.books_and_weather(books, weather_forecast)
-    
+    {
+      "data": {
+        "id": "null",
+        "type": "books",
+        "attributes": {
+          "destination": weather_forecast.name,
+          "forecast": {
+            "summary": weather_forecast.text,
+            "temperature": weather_forecast.temperature 
+          },
+          "total_books_found": books.total_books_found,
+          "books": books.books.map do |book|
+            book_creator(book)
+          end
+        }
+      }
+    }
   end
+  
+  def self.book_creator(book)
+    {
+      "isbn": book[:isbn],
+      "title": book[:title],
+      "publisher": book[:publisher]
+    }
+  end    
 end

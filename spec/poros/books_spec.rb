939ee9409ff2e20819be89101715079859aa4dba
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-23 11:50:24 -0600

    Fix poros test to utilize stymbolized keys like actual poros

--- spec/poros/books_spec.rb
+++ spec/poros/books_spec.rb
@@ -3,19 +3,19 @@ require 'rails_helper'
 RSpec.describe Books do
   it 'instantiates a Books object with the correct attributes from the JSON response' do
     json_data = File.read('spec/fixtures/book_for_city.json')
-    parsed_data = JSON.parse(json_data)
+    parsed_data = JSON.parse(json_data, symbolize_names: true)
 
     books = Books.new(parsed_data)
 
     expect(books).to be_a(Books)
-    expect(books.total_books_found).to eq(parsed_data['numFound'])
+    expect(books.total_books_found).to eq(parsed_data[:numFound])
     
-    expect(books.books.size).to eq(parsed_data['docs'].size) #makes sure same number of books are in the poros as the docs array from json
+    expect(books.books.size).to eq(parsed_data[:docs].size)
 
-    parsed_data['docs'].each_with_index do |book, books_index|
-      expect(books.title(books_index)).to eq(book['title'])
-      expect(books.isbn(books_index)).to eq(book['isbn'] || ["ISBN value not available"])
-      expect(books.publisher(books_index)).to eq(book['publisher'] || ["Publisher not available"])
+    parsed_data[:docs].each_with_index do |book, books_index|
+      expect(books.title(books_index)).to eq(book[:title])
+      expect(books.isbn(books_index)).to eq(book[:isbn] || ["ISBN value not available"])
+      expect(books.publisher(books_index)).to eq(book[:publisher] || ["Publisher not available"])
     end
   end
 end

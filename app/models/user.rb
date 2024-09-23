a3654a93e1a234a698c850082626db51a3f27329
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-23 16:30:32 -0600

    Add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates_presence_of :email, :password_digest, :api_key
+  validates_presence_of :email, :password, :api_key
   
   validates_uniqueness_of :email, :api_key
 

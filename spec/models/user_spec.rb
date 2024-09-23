9ab8ff67f7963f24699b2c12f30a0f3e87cacf29
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-23 17:40:04 -0600

    Update the user_spec.rb file with new class method tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,9 +4,24 @@ RSpec.describe User, type: :model do
   describe "validations" do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password) }
-    it { should validate_presence_of(:api_key) }
 
     it { should validate_uniqueness_of(:email) }
-    it { should validate_uniqueness_of(:api_key) }
+    it { should validate_uniqueness_of(:api_key).allow_nil }
+  end
+
+  describe "instance methods" do
+
+    it "generates an api key before the user is created" do
+      user = User.create!(email: "theguy@theguy.com", password: "notmypassword")
+      expect(user.api_key).not_to be_nil
+      expect(user.api_key.length).to eq(32)
+    end
+
+    it "generates unique api keys for different users" do
+      user1 = User.create!(email: "theotherdude@theotherdude.com", password: "totallymypassword123")
+      user2 = User.create!(email: "maybeaguy@maybeaguy.com", password: "couldbemypassword")
+      
+      expect(user1.api_key).not_to eq(user2.api_key)
+    end
   end
 end

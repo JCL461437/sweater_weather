68c7ad7ec130aefbf7645eb084d92bad58e368f7
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-23 10:34:45 -0600

    Made large changes to the books poros so that it correctly iterates through books from the json[:docs]

--- app/poros/books.rb
+++ app/poros/books.rb
@@ -1,15 +1,29 @@
 class Books
-  attr_reader :total_books_found, :books, 
-              :isbn, :title, :publisher
+  attr_reader :total_books_found, :books
 
   def initialize(data)
-    # Ensure data is a hash and not an array or other type
     data = data.is_a?(Hash) ? data : {}
+    
+    @total_books_found = data['numFound']
+    @books = data['docs'].map do |book|
+      {
+        isbn: book['isbn'] || ["ISBN value not available"],
+        title: book['title'] || "Title not available",
+        publisher: book['publisher'] || ["Publisher not available"]
+      }
+    end
+  end
 
-    @total_books_found = 
-    @books = 
-    @isbn = 
-    @title = 
-    @publisher = 
+  # allows us to call books attributes as if they were instance variables
+  def isbn(index)
+    @books[index][:isbn]
+  end
+
+  def title(index)
+    @books[index][:title]
+  end
+
+  def publisher(index)
+    @books[index][:publisher]
   end
 end

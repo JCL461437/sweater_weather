e76b70402627904c05697ddc904829872beecd0a
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-22 18:32:41 -0600

    refactor the controller to use a facade to format lat long values

--- app/controllers/api/v1/weather_controller.rb
+++ app/controllers/api/v1/weather_controller.rb
@@ -1,16 +1,15 @@
-class Api:V1:WeatherController < ApplicationController
+class Api::V1::WeatherController < ApplicationController
   def forcast  
     location = params[:location] # pulled from query params for `/api/v1/forecast?location=Salt Lake City, UT`
     # {"lat":40.76031,"lng":-111.88822}
 
     # turns location from query params into lat and long using Map Quest API
-    lat_long =  MapQuestApiService.get_lat_long(location)
-
+    lat_long = MapQuestFacade.new.lat_long(location)
     # Retrieves current weather and forcast for hourly and five days using Weather API
     current_weather = WeatherFacade.new.current_weather(lat_long)
-    five_days_forcast = WeatherFacade.new.five_days_forecast(lat_long)
+    five_days_forecast = WeatherFacade.new.five_days_forecast(lat_long)
+    hourly_forecast = WeatherFacade.new.hourly_forecast(lat_long)
 
-    render json: CurrentWeatherSerializer.all_weather(@current_weather)
-    render json: FiveDaysForecastWeatherSerializer.all_weather(@five_days_forecast)
+    render json: WeatherSerializer.all_weather(current_weather, five_days_forecast, hourly_forecast)
   end
 end

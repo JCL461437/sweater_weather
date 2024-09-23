7abfa3616d1ffaeabf7af6ef9d2bafaeb981b21c
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-23 17:44:26 -0600

    Remove superfulous routes in the routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
       get "/forecast", to: "weather#forecast"
-      resources :users, only: [:index, :show, :new, :create]
+      resources :users, only: [:create]
     end
   end
 end

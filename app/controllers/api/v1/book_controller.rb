cda6b681488b64783710de5ace7aca04ad7e403d
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-23 13:26:29 -0600

    Fix flow control in the book controller book_search action, utilizing .blank? instead of .empty?, and removing uncessary conditional statements

--- app/controllers/api/v1/book_controller.rb
+++ app/controllers/api/v1/book_controller.rb
@@ -3,19 +3,18 @@ class Api::V1::BookController < ApplicationController
     location = params[:location]
     quantity = params[:quantity]
 
-    if location.empty?
-      render json: ParametersErrorSerializer.location_error_json, status: 422
-    elsif quantity.empty?
-      render json: ParametersErrorSerializer.quantity_error_json, status: 422
+    if location.blank?
+      render json: ParametersErrorSerializer.location_error_json, status: 404
+      return
+    elsif quantity.blank?
+      render json: ParametersErrorSerializer.quantity_error_json, status: 404
+      return
     end
-
-    if location && quantity != location.empty? && quantity.empty?
-      books = BookFacade.new.get_books(location, quantity)
-
-      lat_long = MapQuestFacade.new.lat_long(location)
-      weather_forecast = WeatherFacade.new.current_weather(lat_long)
       
-      render json: BookWeatherSerializer.books_and_weather(books, weather_forecast), status: 201
-    end
+    books = BookFacade.new.get_books(location, quantity)
+    lat_long = MapQuestFacade.new.lat_long(location)
+    weather_forecast = WeatherFacade.new.current_weather(lat_long)
+      
+    render json: BookWeatherSerializer.books_and_weather(books, weather_forecast), status: 201
   end
 end

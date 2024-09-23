457f32542880daf6911411bec607058681d3b3f9
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-23 16:31:04 -0600

    Add validation tests for user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,8 @@ describe User, type: :model do
   describe "validations" do
     it {should validate_presence_of(:email)}
     it {should validate_presence_of(:password)}
+
     it {should validate_uniqueness_of(:email)}
+    it {should validate_uniqueness_of(:api_key)}
   end
 end

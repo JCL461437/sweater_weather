84255d84ed826c54935b6dbd664771599c33eb79
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-23 11:59:17 -0600

    Add test for no quantity given in params

--- spec/requests/api/v1/book_search_request_spec.rb
+++ spec/requests/api/v1/book_search_request_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "GET /api/v1/book-search?location=denver,co&quantity=5 endpoint" 
       
       lat_long = "40.76031,-111.88822"
       location = "Salt Lake City, UT"
-      quantity = 3
+      quantity = "3"
       
       json_weather_response = File.read('spec/fixtures/five_day_forcast_slc.json')
       stub_request(:get, "/v1/current.json?key=#{Rails.application.credentials.weather[:key]}&q=#{lat_long}").to_return(status: 200, body: json_weather_response)
@@ -29,7 +29,7 @@ RSpec.describe "GET /api/v1/book-search?location=denver,co&quantity=5 endpoint" 
       
       lat_long = "40.76031,-111.88822"
       location = ""
-      quantity = 3
+      quantity = "3"
 
       json_weather_response = File.read('spec/fixtures/five_day_forcast_slc.json')
       weather_stub_request(:get, "/v1/current.json?key=#{Rails.application.credentials.weather[:key]}&q=#{lat_long}").to_return(status: 400, body: json_weather_response)
@@ -44,6 +44,27 @@ RSpec.describe "GET /api/v1/book-search?location=denver,co&quantity=5 endpoint" 
       expect(payload[:errors]).to be_an(Array)
       expect(payload[:errors][0][:status]).to eq("422")
       expect(payload[:errors][0][:detail]).to eq("No location was provided, try again with a location")
+    end
+
+    it "cannot return a payload without a quantity" do 
+      
+      lat_long = "40.76031,-111.88822"
+      location = "Salt Lake City"
+      quantity = ""
+
+      json_weather_response = File.read('spec/fixtures/five_day_forcast_slc.json')
+      weather_stub_request(:get, "/v1/current.json?key=#{Rails.application.credentials.weather[:key]}&q=#{lat_long}").to_return(status: 400, body: json_weather_response)
+      weather_response = JSON.parse(json_weather_response, symbolize_names: true)
+
+      json_book_response = File.read('spec/fixtures/book_for_city.json') # includes limit/quantity of 3
+      book_stub_request(:get, "/search.json?q=#{location}&place:#{location}&limit=#{quantity}").to_return(status: 400, body: json_book_response)
+      book_response = JSON.parse(json_book_response, symbolize_names: true)
+      
+      payload = ParametersErrorSerializer.new.quantity_error_json
+
+      expect(payload[:errors]).to be_an(Array)
+      expect(payload[:errors][0][:status]).to eq("422")
+      expect(payload[:errors][0][:detail]).to eq("No quantity was provided, try again with a quantity")
     end
   end
 end

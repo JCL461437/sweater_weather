f7eb8af4bf95e6c19e4e31c9728e63ffee8bc60a
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-23 11:44:53 -0600

    Added correct status for correct json

--- app/controllers/api/v1/book_controller.rb
+++ app/controllers/api/v1/book_controller.rb
@@ -15,7 +15,7 @@ class Api::V1::BookController < ApplicationController
       lat_long = MapQuestFacade.new.lat_long(location)
       weather_forecast = WeatherFacade.new.current_weather(lat_long)
       
-      render json: BookWeatherSerializer.books_and_weather(books, weather_forecast)
+      render json: BookWeatherSerializer.books_and_weather(books, weather_forecast), status: 201
     end
   end
 end

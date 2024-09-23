4e3f3f41780708b4b93e999c746d0c8b917be626
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-23 16:31:56 -0600

    Add method for generating api key and before_create in user.rb model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ApplicationRecord
   validates_uniqueness_of :email, :api_key
 
   has_secure_password
+
+  before_create :api_key
+
+  has_secure_password
+
+  private
+  
+  def api_key
+    self.api_key = SecureRandom.hex
+  end
 end

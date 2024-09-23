8115e849581ccd3103fc0b8d3c83167d9cf32949
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-23 17:40:51 -0600

    Add user.rb refined create_api_key instance method and changes to the validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,16 @@ class User < ApplicationRecord
   has_secure_password
   
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
-  validates :api_key, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :api_key, uniqueness: true, allow_nil: true
   
   before_create :create_api_key
 
   private
   
   def create_api_key
-    self.api_key = SecureRandom.hex(16)
+    if self.api_key.nil?
+      self.api_key = SecureRandom.hex(16)
+    end
   end
 end

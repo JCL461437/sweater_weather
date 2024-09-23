19014ee2bb894dc21bca6be2e5e438a6166cce7d
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-22 18:09:12 -0600

    Fix the weather_facade file from typos

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -8,15 +8,15 @@ class WeatherFacade
     json = WeatherApiService.get_five_days_forecast(lat_long) # hitting methods in service
 
     json[:forecast][:forecastday].map do |forecasted_day| # iterating through json array
-      FiveDaysForcastWeather.new(forecasted_day) # making poros
+      FiveDaysForecastWeather.new(forecasted_day) # making poros
     end
   end
 
-  # def hourly_forecast(lat_long)
-  #   json = WeatherApiService.get_five_days_forecast(lat_long) # hitting methods in service
+  def hourly_forecast(lat_long)
+    json = WeatherApiService.get_five_days_forecast(lat_long) # hitting methods in service
 
-  #   json[:forecast][:forecastday][0][:hour].map do |forecasted_hour|
-  #     HourForecastWeather.new(forecasted_hour)
-  #   end
-  # end
+    json[:forecast][:forecastday][0][:hour].map do |forecasted_hour|
+      HourForecastWeather.new(forecasted_hour)
+    end
+  end
 end

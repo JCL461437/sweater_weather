e36ee40d4fba3c5e071f9010eec14ff9aac89873
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-23 11:43:58 -0600

    Update the BookController with flow control in the event query params are not correctly filled out

--- app/controllers/api/v1/book_controller.rb
+++ app/controllers/api/v1/book_controller.rb
@@ -3,11 +3,19 @@ class Api::V1::BookController < ApplicationController
     location = params[:location]
     quantity = params[:quantity]
 
-    books = BookFacade.new.get_books(location, quantity)
+    if location == nil
+      render json: ParametersErrorSerializer.location_error_json, status: 422
+    elsif quantity == nil
+      render json: ParametersErrorSerializer.quantity_error_json, status: 422
+    end
 
-    lat_long = MapQuestFacade.new.lat_long(location)
-    weather_forecast = WeatherFacade.new.current_weather(lat_long)
-    
-    render json: BookWeatherSerializer.books_and_weather(books, weather_forecast)
+    if location && quantity != nil
+      books = BookFacade.new.get_books(location, quantity)
+
+      lat_long = MapQuestFacade.new.lat_long(location)
+      weather_forecast = WeatherFacade.new.current_weather(lat_long)
+      
+      render json: BookWeatherSerializer.books_and_weather(books, weather_forecast)
+    end
   end
 end

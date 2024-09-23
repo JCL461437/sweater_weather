d90b4353a63e810caca3ea4c46e313c4d6c309d8
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-23 09:43:04 -0600

    Add the books.rb poros file with outline for json

---
+++ app/poros/books.rb
@@ -0,0 +1,15 @@
+class Books
+  attr_reader :total_books_found, :books, 
+              :isbn, :title, :publisher
+
+  def initialize(data)
+    # Ensure data is a hash and not an array or other type
+    data = data.is_a?(Hash) ? data : {}
+
+    @total_books_found
+    @books = 
+    @isbn = 
+    @title
+    @publisher
+  end
+end

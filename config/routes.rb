56dcc5126ec1edd4156339fd82151637cfca36dd
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-23 11:15:05 -0600

    Add the endpoint /book-search mapping to the book controller book_search action in the routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
       get "/forecast", to: "weather#forecast"
+      get "/book-search", to: "book#book_search"
     end
   end
 end

c095eee139ad6bf328cc0fb00425d2e3c153dca8
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-23 17:39:30 -0600

    Add shoulda::matchers block to the rails_helper file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,12 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'shoulda/matchers'
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production

09e70dd80b3c804b72cd1c9231b42bf4cc3ba414
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-23 10:46:25 -0600

    Switch the user of the five days forcast to the current weather for the users selected location

--- app/controllers/api/v1/book_controller.rb
+++ app/controllers/api/v1/book_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::BookController < ApplicationController
     books = BookFacade.new.get_books(location, quantity)
 
     lat_long = MapQuestFacade.new.lat_long(location)
-    weather_forecast = WeatherFacade.new.five_days_forecast(lat_long)
+    weather_forecast = WeatherFacade.new.current_weather(lat_long)
     
     render json: BookWeatherSerializer.books_and_weather(books, weather_forecast)
   end

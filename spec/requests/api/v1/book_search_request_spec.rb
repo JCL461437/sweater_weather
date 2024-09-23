3e556ffb2c175da845a55541b1de95f8d854d8b1
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-23 09:04:44 -0600

    Add empty spec file for the book_search request

---
+++ spec/requests/api/v1/book_search_request_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe "will return city, forecast, and book information for the GET /api/v1/book-search?location=denver,co&quantity=5 endpoint" do
+  it '' do
+
+  end
+end

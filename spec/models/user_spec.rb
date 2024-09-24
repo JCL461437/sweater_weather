7ae1fc3e4e33697684fea6ee949f1f9e12fc0d75
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-24 13:41:01 -0600

    Add user_spec.rb model test changes to validations tests to reflect the model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe User, type: :model do
   describe "validations" do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password) }
+    it { should validate_presence_of(:password_confirmation) }
 
     it { should validate_uniqueness_of(:email) }
     it { should validate_uniqueness_of(:api_key).allow_nil }
@@ -12,14 +13,14 @@ RSpec.describe User, type: :model do
   describe "instance methods" do
 
     it "generates an api key before the user is created" do
-      user = User.create!(email: "theguy@theguy.com", password: "notmypassword")
+      user = User.create!(email: "theguy@theguy.com", password: "notmypassword", password_confirmation: "notmypassword")
       expect(user.api_key).not_to be_nil
       expect(user.api_key.length).to eq(32)
     end
 
     it "generates unique api keys for different users" do
-      user1 = User.create!(email: "theotherdude@theotherdude.com", password: "totallymypassword123")
-      user2 = User.create!(email: "maybeaguy@maybeaguy.com", password: "couldbemypassword")
+      user1 = User.create!(email: "theotherdude@theotherdude.com", password: "totallymypassword123", password_confirmation: "totallymypassword123")
+      user2 = User.create!(email: "maybeaguy@maybeaguy.com", password: "couldbemypassword", password_confirmation: "couldbemypassword")
       
       expect(user1.api_key).not_to eq(user2.api_key)
     end

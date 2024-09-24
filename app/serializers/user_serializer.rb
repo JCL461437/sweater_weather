89ef51a41eb309ab87f2fc4b5d45fecf4eb4bda6
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-24 13:40:38 -0600

    Edit serializer to use gem syntax instead of hand rolling

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,16 +1,5 @@
 class UserSerializer
   include JSONAPI::Serializer
 
-  def self.user_information(user)
-    {
-      "data": {
-        "type": "user",
-        "id": "#{user.id}",
-        "attributes": {
-          "email": user.email,
-          "api_key": user.api_key
-        }
-      }
-    }
-  end
+  attributes :email, :api_key
 end

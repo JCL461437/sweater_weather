1fb6e6286436da4bca5c73d71471a93099273a6b
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-24 12:21:02 -0600

    Add strong params with require :user key from json. Add use of serializers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,16 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    
+    user = User.create(user_params)
+
+    if user.save
+      render json: UserSerializer.new(user)
+    else
+      render json: JsonErrorSerializer.bad_user_input
+    end
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end

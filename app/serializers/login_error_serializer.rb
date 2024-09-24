cbc9fe3b3a0bb018c9a2d668985ed9e284b07692
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-24 15:51:06 -0600

    Add error_response method to the serializer

--- app/serializers/login_error_serializer.rb
+++ app/serializers/login_error_serializer.rb
@@ -1,13 +1,11 @@
 class LoginErrorSerializer 
   include JSONAPI::Serializer
-  def self.error 
+  def self.error_response
     {
-      errors: [
-        {
-          status: '401',
-          detail: "Invalid credentials, please re-enter and try again."
-        }
-      ]
+      errors: {
+        status: '401',
+        detail: "Invalid credentials, please re-enter and try again."
+      }
     }
   end
 end

be2923290569feb41666b48ac65623667a3269d2
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-24 13:54:31 -0600

    Add sessions create route to the routes.rb file

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::SessionsController < ApplicationController
-  def 
-    
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome, #{user.first_name}!"
+      redirect_to user_dashboard_index_path(user.id)
+    else
+      flash[:error] = "Sorry, your credentials are bad."
+      render :login_form
   end
 end

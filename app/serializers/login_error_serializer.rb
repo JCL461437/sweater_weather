dd5f9e31e93057bb58c44ba09aa7d0594e4fcca3
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-24 15:30:57 -0600

    Add handrolled json within class method

--- app/serializers/login_error_serializer.rb
+++ app/serializers/login_error_serializer.rb
@@ -1,11 +1,13 @@
 class LoginErrorSerializer 
   include JSONAPI::Serializer
-  {
-    errors: [
-      {
-        status: '401',
-        detail: "Invalid credentials, please re-enter and try again."
-      }
-    ]
-  }
+  def self.error 
+    {
+      errors: [
+        {
+          status: '401',
+          detail: "Invalid credentials, please re-enter and try again."
+        }
+      ]
+    }
+  end
 end

97a5a2140a04d8fb52c6f5291583130e1d1b7ae1
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-24 15:30:31 -0600

    Add error method to the rendering of LoginErrorSerializer

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,9 +3,9 @@ class Api::V1::SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
 
     if user && user.authenticate(params[:password])
-      render json: UserSerializer.new(user)
+      render json: UserSerializer.new(user), status: 200
     else
-      render json: LoginErrorSerializer.new
+      render json: LoginErrorSerializer.new.error, status: 401
     end
   end
 end

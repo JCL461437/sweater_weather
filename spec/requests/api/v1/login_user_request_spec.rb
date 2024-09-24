59da7fe6004af8e5c180b0bccc46d9878462eb4f
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-24 13:48:12 -0600

    Add login_user_request_spec.rb file

---
+++ spec/requests/api/v1/login_user_request_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe 'User Creation Request for POST /api/v1/users endpoint' do
+  describe "happy path" do
+
+  end
+
+  describe "sad path" do
+    
+  end
+end

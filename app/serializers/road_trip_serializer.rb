8a6768cd02f6132a299e336814036b6604dfbccf
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-25 08:37:25 -0600

    Add some key value assignment in the road_trip_serializer

--- app/serializers/road_trip_serializer.rb
+++ app/serializers/road_trip_serializer.rb
@@ -6,9 +6,9 @@ class RoadTripSerializer
           "id": "null",
           "type": "road_trip",
           "attributes": {
-              "start_city": ,
-              "end_city": ,
-              "travel_time": ,
+              "start_city": road_trip.start_city,
+              "end_city": road_trip.end_city,
+              "travel_time": road_trip.travel_time,
               "weather_at_eta": {
                   "datetime": ,
                   "temperature": ,

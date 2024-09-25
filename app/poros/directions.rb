a77162fc6763aabda2c37f1142e3e8ac138338ef
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-25 08:20:58 -0600

    Added attr_reader values for initialize instance variables

--- app/poros/directions.rb
+++ app/poros/directions.rb
@@ -1,10 +1,9 @@
 class Directions
-  attr_reader :
+  attr_reader :start_city, :end_city, :travel_time
 
   def initialize(json, origin, destination)
     @start_city = origin
     @end_city = destination
     @travel_time = json[:route][:formattedTime]
-
   end
 end

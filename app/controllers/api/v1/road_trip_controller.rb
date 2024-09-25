4455ae65cd86303535e7c71f58cfc096deb2f757
Jan Lehigh <janlehigh461437@gmail.com>
2024-09-25 08:13:08 -0600

    Update road trip controller create action with use of MapQuestFacade

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,5 +1,17 @@
 class Api::V1::RoadTripController < ApplicationController
-  def create
+  before_action :authenticate_api_key
 
+  def create
+    origin = params[:origin]
+    destination = params[:destination]
+
+    MapQuestFacade.new.directions(origin, destination)
+
+  end
+
+  private
+
+  def trip_params
+    params.permit(:origin, :destination, :api_key)
   end
 end
